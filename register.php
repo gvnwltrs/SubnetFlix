16dbd55114cf6f409578d0251516af144ef4f5d4
Gavin Walters <waltersg298@gmail.com>
2022-08-23 23:43:46 -0400

    initial commit

---
+++ register.php
@@ -0,0 +1,39 @@
+<?php 
+if(isset($_POST["submitButton"])) {
+    echo "this form was submitted"; 
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome to SubnetFlix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+
+<body>
+
+    <div class="signInContainer">
+        <div class="column">
+            <form method="POST">
+                <input type="text" name="firstName" placeholder="First name" required> 
+                <input type="text" name="lastName" placeholder="Last name" required> 
+
+                <input type="text" name="username" placeholder="Username" required> 
+
+                <input type="email" name="email" placeholder="Email" required> 
+                <input type="email" name="email2" placeholder="Confirm email" required> 
+
+                <input type="password" name="password" placeholder="password" required> 
+                <input type="password" name="password2" placeholder="Confirm password" required> 
+
+                <input type="submit" name="submitButton" value="SUBMIT">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
