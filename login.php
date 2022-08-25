1c0db717f87d373cbf90da4fe87a621d0620779b
Gavin Walters <waltersg298@gmail.com>
2022-08-25 07:51:17 -0400

    finished front-end for register/login page

--- login.php
+++ login.php
@@ -19,26 +19,19 @@ if(isset($_POST["submitButton"])) {
 
             <div class="header">
                 <img src="assets/images/subnetflix-logo.png" title="Logo" alt="Site Logo">
-                <h3>Sign Up</h3>
+                <h3>Sign In</h3>
                 <span>to continue to SubnetFlix</span>
             </div>
 
             <form method="POST">
-                <input type="text" name="firstName" placeholder="First name" required> 
-                <input type="text" name="lastName" placeholder="Last name" required> 
-
                 <input type="text" name="username" placeholder="Username" required> 
 
-                <input type="email" name="email" placeholder="Email" required> 
-                <input type="email" name="email2" placeholder="Confirm email" required> 
-
                 <input type="password" name="password" placeholder="password" required> 
-                <input type="password" name="password2" placeholder="Confirm password" required> 
 
                 <input type="submit" name="submitButton" value="SUBMIT">
             </form>
 
-            <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
+            <a href="register.php" class="signInMessage">Don't have an account? Sign up here!</a>
         </div>
     </div>
 
