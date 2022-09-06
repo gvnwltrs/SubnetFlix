472c11f6018f65923d463cb4926de277e12814ab
Gavin Walters <waltersg298@gmail.com>
2022-09-06 16:33:11 -0600

    Added features to update a user's video progress every 3 seconds and reset status upon end of video (finished column sets upon end of video). User progress grabbed upon running video and video. Video will start where user has left off.

--- ajax/addDuration.php
+++ ajax/addDuration.php
@@ -2,11 +2,13 @@
 require_once("../includes/config.php"); 
 
 if(isset($_POST["videoId"]) && isset($_POST["username"])) {
+    // using this to check if row exists for current user and video 
     $query = $connection->prepare("SELECT * FROM videoProgress WHERE videoId=:videoId AND username=:username" ); 
     $query->bindValue(":videoId", $_POST["videoId"]); 
     $query->bindValue(":username", $_POST["username"]); 
     $query->execute(); 
 
+    // if the user/video entry doesn't exist, make it now 
     if($query->rowCount() == 0) {
         $query = $connection->prepare("INSERT INTO videoProgress (username, videoId)
                                         VALUES(:username, :videoId)"); 
